4f912de5d0fd11bbddc28d5122439cdcf2c4a66b
Tanmayjain0906 <tanmayjain983@gmail.com>
2024-01-24 18:58:39 +0530

    crypto

--- src/components/Coin/TogglePriceType.js/index.js
+++ src/components/Coin/TogglePriceType.js/index.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import "./style.css"
 
-function TogglePriceType({priceType, handleChangePriceType}) {
+function TogglePriceType({ priceType, handleChangePriceType }) {
 
   return (
     <div className='type-btn-container'>
-        <button id='btn-1' className={priceType == "prices" ? "active" : "btns"} onClick={() => handleChangePriceType("prices")}>Price</button>
-        <button id='btn-2' className={priceType == "market_caps" ? "active" : "btns"} onClick={() => handleChangePriceType("market_caps")}>Market Cap</button>
-        <button id='btn-3' className={priceType == "total_volumes" ? "active" : "btns"} onClick={() => handleChangePriceType("total_volumes")}>Total Volume</button>
+      <button id='btn-1' className={priceType == "prices" ? "active-btns" : "btns"} onClick={() => handleChangePriceType("prices")}>Price</button>
+      <button id='btn-2' className={priceType == "market_caps" ? "active-btns" : "btns"} onClick={() => handleChangePriceType("market_caps")}>Market Cap</button>
+      <button id='btn-3' className={priceType == "total_volumes" ? "active-btns" : "btns"} onClick={() => handleChangePriceType("total_volumes")}>Total Volume</button>
     </div>
   )
 }
